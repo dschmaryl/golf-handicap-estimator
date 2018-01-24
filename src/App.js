d4173bbec6bcf90134e60b2597604e2f051ae265
dschmaryl <dschmaryl@gmail.com>
2018-01-24 00:10:29 -0500

    and with this groove...

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 export class App extends React.Component {
   render() {
     return (
-      <p>in the beginning, there was jack</p>
+      <p>in the beginning, there was jack, and jack had a groove</p>
     );
   }
 }
