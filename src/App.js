91594534a0b1bedec5bcab38e30f1f8751c0353f
dschmaryl <dschmaryl@gmail.com>
2018-01-24 00:31:00 -0500

    and house music was born

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export class App extends React.Component {
     return (
       <div>
         <p>in the beginning, there was jack, and jack had a groove.</p>
-        <p>"Let there be house!"</p>
+        <p>"Let there be HOUSE!"</p>
       </div>
     );
   }
