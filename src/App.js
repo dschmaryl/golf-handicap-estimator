e9809139fb6cefcf390e55bf4a64b51fdb009474
dschmaryl <dschmaryl@gmail.com>
2018-01-24 11:45:24 -0500

    add a little styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
 export class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
   render() {
     return (
-      <div>
-        <p>In the beginning, there was jack, and jack had a groove.</p>
-        <p>And from this groove came the groove of all grooves.</p>
-        <br />
-        <p>"Let there be HOUSE!"</p>
+      <div className="body">
+        <header>
+          <h3>In the beginning, there was jack and jack had a groove.</h3>
+        </header>
+        <article>
+          <p>And from this groove came the groove of all grooves.</p>
+          <br />
+          <h4><em>"Let there be HOUSE!"</em></h4>
+        </article>
       </div>
     );
   }
