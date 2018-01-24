901b41f0fcec3913a00e066ecec6d8e8bdd66fee
dschmaryl <dschmaryl@gmail.com>
2018-01-24 00:34:51 -0500

    this is my house

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ export class App extends React.Component {
   render() {
     return (
       <div>
-        <p>in the beginning, there was jack, and jack had a groove.</p>
-        <p>"Let there be HOUSE!"</p>
+        <p>In the beginning, there was jack, and jack had a groove.</p>
+        <p>And from this groove came the groove of all grooves.</p>
       </div>
     );
   }
