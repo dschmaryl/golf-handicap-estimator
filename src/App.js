5a351a38c56c148351af4fa1935d1b0553cd9c4d
dschmaryl <dschmaryl@gmail.com>
2018-01-24 00:22:05 -0500

    testering

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import React from 'react';
 export class App extends React.Component {
   render() {
     return (
-      <p>in the beginning, there was jack, and jack had a groove</p>
+      <div>
+        <p>in the beginning, there was jack, and jack had a groove.</p>
+        <p>"Let there be house!"</p>
+      </div>
     );
   }
 }
