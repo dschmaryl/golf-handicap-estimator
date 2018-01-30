5ba7b322f0860c965546cad8d86f31e5d378e99d
dschmaryl <dschmaryl@gmail.com>
2018-01-30 16:39:10 -0500

    use str replace for plus handicap

--- src/components/Handicap.js
+++ src/components/Handicap.js
@@ -24,13 +24,9 @@ export function Handicap(props) {
     if (scores.length === 0) {
       return 'Please enter at least one score and as many as 20.';
     } else {
-      let handicap = Math.min(calcHandicap(scores), 50);
-      if (handicap < 0) {
-        handicap = Math.min(Math.abs(handicap), 5);
-        return `Your handicap is approximately: +${handicap}`;
-      } else {
-        return `Your handicap is approximately: ${handicap}`;
-      }
+      const handicapNum = Math.max(-5, Math.min(calcHandicap(scores), 50));
+      const handicapStr = ('' + handicapNum).replace('-', '+')
+      return 'Your handicap is approximately: ' + handicapStr;
     }
   }
 
