a358c4f3384d878cce18ede48cde1a55a515c6c4
dschmaryl <dschmaryl@gmail.com>
2019-05-08 13:45:14 -0400

    Updates

---
+++ precache-manifest.843ef91a9220770585aaa4de256ec174.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e34fee51bfafa3587b5e4e759fafb7f",
+    "url": "/golf-handicap-estimator/index.html"
+  },
+  {
+    "revision": "b76c78470f615840dd0b",
+    "url": "/golf-handicap-estimator/static/css/main.c1335445.chunk.css"
+  },
+  {
+    "revision": "889701e3c5e2ae4481b9",
+    "url": "/golf-handicap-estimator/static/js/2.a278b9b3.chunk.js"
+  },
+  {
+    "revision": "b76c78470f615840dd0b",
+    "url": "/golf-handicap-estimator/static/js/main.51735081.chunk.js"
+  },
+  {
+    "revision": "b640c43e9a0e217ded52",
+    "url": "/golf-handicap-estimator/static/js/runtime~main.67a2f0cd.js"
+  }
+]);
