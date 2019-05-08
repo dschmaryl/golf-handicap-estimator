96b3bbfc4b45570f682db49a53be6955296093ee
dschmaryl <dschmaryl@gmail.com>
2019-05-08 13:51:39 -0400

    lose unnecessary type

--- src/types.ts
+++ src/types.ts
@@ -10,7 +10,3 @@ export interface State {
   rounds: Rounds;
   handicap: number;
 }
-
-// export type Action =
-//   | { type: 'ADD_ROUND'; round: Round }
-//   | { type: 'REMOVE_ROUND'; index: number };
